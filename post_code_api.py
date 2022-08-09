e67737e03d9fd7759e9b809d547fe964c22ac2b6
ayan <ayanie@live.co.uk>
2022-08-09 15:55:01 +0100

    1600 update

--- post_code_api.py
+++ post_code_api.py
@@ -27,43 +27,43 @@ result_dict = response_dict ['result'] #resonse_dict = data
 
 #print(result_dict)
 
-for key in result_dict.keys(): # change to indexing
-    if key == 'postcode' :
-        #print(f"please confirm this is your postcode is {result_dict.get('postcode')}")
-        print(f"please confirm this is your postcode is {result_dict[key]}")
-    elif key == 'longitude':
-        #print(f"your longitude is {result_dict.get('longitude')}")
-        print(f"your longitude is {result_dict[key]}")
-    elif key == 'latitude':
-        #print(f"your latitude is {result_dict.get('latitude')}")
-         print(f"your latitude is {result_dict[key]}")
-    else:
-        pass
+# for key in result_dict.keys(): # change to indexing
+#     if key == 'postcode' :
+#         #print(f"please confirm this is your postcode is {result_dict.get('postcode')}")
+#         print(f"please confirm this is your postcode is {result_dict[key]}")
+#     elif key == 'longitude':
+#         #print(f"your longitude is {result_dict.get('longitude')}")
+#         print(f"your longitude is {result_dict[key]}")
+#     elif key == 'latitude':
+#         #print(f"your latitude is {result_dict.get('latitude')}")
+#          print(f"your latitude is {result_dict[key]}")
+#     else:
+#         pass
 
 
 # once completed - create a function to do return the required value - 1 function MUST only REtURN 1 VALUE
 
-def postcode():
-    for key in result_dict.keys():
-        if key == 'postcode':
-            return (f"please confirm this is your postcode {result_dict[key]}")
-
-print(postcode())
-
-
-def longitude():
-    for key in result_dict.keys():
-        if key == 'longitude':
-            return (f"your longitude is {result_dict[key]}")
-
-print(longitude())
-
-def latitude():
-    for key in result_dict.keys():
-        if key == 'latitude':
-            return (f"your latitude is {result_dict[key]}")
-
-print(latitude())
+# def postcode():
+#     for key in result_dict.keys():
+#         if key == 'postcode':
+#             return (f"please confirm this is your postcode {result_dict[key]}")
+#
+# print(postcode())
+#
+#
+# def longitude():
+#     for key in result_dict.keys():
+#         if key == 'longitude':
+#             return (f"your longitude is {result_dict[key]}")
+#
+# print(longitude())
+#
+# def latitude():
+#     for key in result_dict.keys():
+#         if key == 'latitude':
+#             return (f"your latitude is {result_dict[key]}")
+#
+# print(latitude())
 
 
 
@@ -72,7 +72,32 @@ print(latitude())
 # create a class with all of these functions
 
 class Address():
+    def __init__(self):
+        self.postcode  # result_dict.get('postcode')
 
-# create a file called postcode_checker.py
-# import this file and class
-# call these functions in postcode_check.py
+    #def is_valid(self):
+        #user_input = input('please enter your postcode ')
+        #if user_input != self.postcode():
+            #print("Try again as we dont recognise this")
+
+    def postcode(self):
+       for key in result_dict.keys():
+            if key == 'postcode':
+                return (f"please confirm this is your postcode {result_dict[key]}")
+
+    def longitude(self):
+        for key in self.result_dict.keys():
+            if key == 'longitude':
+                return (f"your longitude is {result_dict[key]}")
+
+    def latitude(self):
+        for key in result_dict.keys():
+            if key == 'latitude':
+                return (f"your latitude is {result_dict[key]}")
+
+address_object = Address()
+
+# print(address_object.is_valid())
+# print(address_object.postcode())
+# print(address_object.latitude())
+# print(address_object.latitude())
--- postcode_checker.py
+++ postcode_checker.py
@@ -1,3 +1,16 @@
+# create a file called postcode_checker.py
+# import this file and class
+# call these functions in postcode_check.py
 
+from post_code_api import  Address
 
-from post_code_api import Address
+class Post(Address):
+    def __init__(self):
+        super().__init__()
+
+    def hi(self):
+        return "hi"
+
+checker_object = Post()
+print(checker_object.hi())
+print(checker_object.postcode()) 
